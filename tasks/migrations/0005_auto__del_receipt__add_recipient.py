6afc00db05568e8a14592c6bcbd137651c8e350c
Sebastian Serrano <sebastian.serrano@gmail.com>
2013-03-03 20:08:38 -0300

    missing task migration

---
+++ tasks/migrations/0005_auto__del_receipt__add_recipient.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Deleting model 'Receipt'
+        db.delete_table(u'tasks_receipt')
+
+        # Adding model 'Recipient'
+        db.create_table(u'tasks_recipient', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('from_user', self.gf('django.db.models.fields.related.ForeignKey')(related_name='tasks_sent', to=orm['accounts.User'])),
+            ('to_user', self.gf('django.db.models.fields.related.ForeignKey')(related_name='tasks_received', to=orm['accounts.User'])),
+            ('task', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['tasks.Task'])),
+        ))
+        db.send_create_signal(u'tasks', ['Recipient'])
+
+
+    def backwards(self, orm):
+        # Adding model 'Receipt'
+        db.create_table(u'tasks_receipt', (
+            ('to_user', self.gf('django.db.models.fields.related.ForeignKey')(related_name='tasks_received', to=orm['accounts.User'])),
+            ('task', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['tasks.Task'])),
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('from_user', self.gf('django.db.models.fields.related.ForeignKey')(related_name='tasks_sent', to=orm['accounts.User'])),
+        ))
+        db.send_create_signal(u'tasks', ['Receipt'])
+
+        # Deleting model 'Recipient'
+        db.delete_table(u'tasks_recipient')
+
+
+    models = {
+        u'accounts.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'remote_user': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'tasks.attachment': {
+            'Meta': {'object_name': 'Attachment'},
+            'file': ('django.db.models.fields.files.FileField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'task': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['tasks.Task']"})
+        },
+        u'tasks.quote': {
+            'Meta': {'object_name': 'Quote'},
+            'deliver_date': ('django.db.models.fields.DateTimeField', [], {}),
+            'details': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'price': ('django.db.models.fields.IntegerField', [], {}),
+            'status': ('django.db.models.fields.CharField', [], {'max_length': '2'}),
+            'task': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['tasks.Task']"}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['accounts.User']"})
+        },
+        u'tasks.recipient': {
+            'Meta': {'object_name': 'Recipient'},
+            'from_user': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'tasks_sent'", 'to': u"orm['accounts.User']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'task': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['tasks.Task']"}),
+            'to_user': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'tasks_received'", 'to': u"orm['accounts.User']"})
+        },
+        u'tasks.task': {
+            'Meta': {'object_name': 'Task'},
+            'accepted_by': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'taken_tasks'", 'null': 'True', 'to': u"orm['accounts.User']"}),
+            'archived': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'bounty': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'can_change': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'can_forward': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            'expire': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'involved': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['accounts.User']", 'symmetrical': 'False'}),
+            'sender': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'created_tasks'", 'to': u"orm['accounts.User']"}),
+            'status': ('django.db.models.fields.CharField', [], {'max_length': '2'}),
+            'task_id': ('django.db.models.fields.CharField', [], {'max_length': '36'})
+        },
+        u'tasks.update': {
+            'Meta': {'object_name': 'Update'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'message': ('django.db.models.fields.TextField', [], {}),
+            'quote': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['tasks.Quote']", 'null': 'True', 'blank': 'True'}),
+            'task': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['tasks.Task']"}),
+            'timestamp': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'update_type': ('django.db.models.fields.CharField', [], {'max_length': '2'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['accounts.User']"})
+        }
+    }
+
+    complete_apps = ['tasks']
