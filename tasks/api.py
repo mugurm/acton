417a92288db03f114b33453f8b0cadac9d3cf842
Sebastian Serrano <sebastian.serrano@gmail.com>
2013-03-03 20:01:07 -0300

    more api tweaks

--- tasks/api.py
+++ tasks/api.py
@@ -2,9 +2,26 @@ from tastypie import fields
 from tastypie.api import Api
 from tastypie.resources import ModelResource
 
+try:
+    from django.contrib.auth import get_user_model
+except ImportError: # django < 1.5
+    from django.contrib.auth.models import User
+else:
+    User = get_user_model()
+
 from .models import Task, Update, Receipt
 
+class UserResource(ModelResource):
+    class Meta:
+        queryset = User.objects.all()
+
+    def dehydrate(self, bundle):
+        bundle.data['display_name'] = bundle.obj.username 
+        return bundle
+
+
 class TaskResource(ModelResource):
+    sender = fields.ForeignKey(UserResource, 'sender', full=True)
     updates = fields.ToManyField('tasks.api.UpdateResource', 'update_set', null=True, full=True)
     receipts = fields.ToManyField('tasks.api.ReceiptResource', 'receipt_set', null=True, full=True)
 
@@ -25,6 +42,8 @@ class UpdateResource(ModelResource):
         queryset = Update.objects.all()
 
 class ReceiptResource(ModelResource):
+    from_user = fields.ForeignKey(UserResource, 'from_user', full=True)
+    to_user = fields.ForeignKey(UserResource, 'to_user', full=True)
     task = fields.ForeignKey(TaskResource, 'task')
     class Meta:
         queryset = Receipt.objects.all()
@@ -33,3 +52,4 @@ class ReceiptResource(ModelResource):
 v1_api = Api(api_name='v1')
 v1_api.register(TaskResource())
 v1_api.register(UpdateResource())
+v1_api.register(UserResource())
